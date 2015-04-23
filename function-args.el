0e072e93d231497389e06b50e4070d879049e767
Oleh Krehel <ohwoeowho@gmail.com>
2015-04-23 16:13:55 +0200

    function-args.el (moo-select-candidate): Update

--- function-args.el
+++ function-args.el
@@ -962,7 +962,7 @@ When PREFIX is not nil, erase it before inserting."
            (require 'ivy)
            (let* ((ivy-height 20)
                   (res (ivy-read "tag: " (mapcar #'car candidates)
-                                 nil nil nil preselect)))
+                                 :preselect preselect)))
              (when res
                (funcall action
                         (cdr (assoc res candidates))))))
